c79d5c34e4ffc04850aa7e770b86c7894ffc8d62
yuriscuba <ganya03dfm@gmail.com>
2026-02-10 21:36:28 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- AGREGA ESTA LÍNEA (con el punto inicial)
 import App from './App';
 import LoadingSpinner from './components/LoadingSpinner';
 
